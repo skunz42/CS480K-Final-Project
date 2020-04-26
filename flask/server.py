27fee0e5def5b96f0280b29828b012b8534505e3
Evan Moeller <evancmoeller@gmail.com>
2020-04-25 23:31:27 -0400

    Finished server.py?

--- flask/server.py
+++ flask/server.py
@@ -1,5 +1,6 @@
 from flask import Flask, request, render_template, send_from_directory
 import os
+from result_generator import ResultGenerator
 
 app = Flask(__name__)
 
@@ -11,9 +12,9 @@ def index():
 def generate_result():
 	city_name = request.form['city_name']
 	state_abrv = request.form['state_abrv']
-	print(city_name)
-	print(state_abrv)
-	#ToDo: Send "city_name" and "state_abrv" to function here
+	#print(city_name)
+	#print(state_abrv)
+	ResultGenerator(city_name, state_abrv)
 	return render_template('result.html')
 
 
