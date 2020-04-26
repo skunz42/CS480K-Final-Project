5256870882bff6e94f63b9e5889cc6fe63d81bd3
Evan Moeller <evancmoeller@gmail.com>
2020-04-25 20:48:05 -0400

    Added declarations for HTTP Handler, storing as backup

--- web_gui/server.py
+++ web_gui/server.py
@@ -1,11 +1,19 @@
 import http.server
-import socketserver
 
 PORT = 8080
-Handler = http.server.SimpleHTTPRequestHandler
+
+class Handler(http.server.BaseHTTPRequestHandler):
+	def do_GET(self):
+		pass
+
+	def do_PUT(self):
+		pass
+
+#Temporary replace later once custom handler is working
+TmpHandler = http.server.SimpleHTTPRequestHandler
 
 if __name__ == '__main__':
     from http.server import HTTPServer
-    server = HTTPServer(('localhost', 8080), Handler)
+    server = HTTPServer(('localhost', 8080), TmpHandler)
     print('Starting server, use <Ctrl-C> to stop')
     server.serve_forever()
