e52b79dbc2a3e3eeea94d6883ac9b19f19fbda15
skunz42 <skunz598@gmail.com>
2020-05-03 17:20:46 -0400

    Added some new features

--- src/web/server.js
+++ src/web/server.js
@@ -22,63 +22,26 @@ var router = function(request, response) {
     }
 };
 
-var stores = function (request, response) {
+var stores = async function (request, response) {
     var reqURI = url.parse("http://" + request.headers.host + request.url);
     var params = reqURI.query;
     var tms = params.substr(params.indexOf('=') + 1);
     var terms = tms.replace("%20", " ");
     console.log(terms)
-    var MongoClient = require('mongodb').MongoClient;
-    MongoClient.connect("mongodb://localhost", function(err, client) {
-      if(err) { throw err; }
-      var db = client.db('CS432FP')
-      db.collection('storeinfo').find({City:terms}).toArray(function (findErr, result) {
-        if (findErr) throw findErr;
-        response.writeHead(200, {
-            'Content-Type':'application/json',
-            'Access-Control-Allow-Origin':'*'
-        });
-        response.write(JSON.stringify(result));
-        response.end();
-        client.close();
+    const {MongoClient} = require('mongodb');
+    const client = new MongoClient(/*key here*/);
+    await client.connect();
+    var db = client.db('CS480KFP')
+    db.collection('stores').find({City:terms}).toArray(function (findErr, result) {
+      if (findErr) throw findErr;
+      response.writeHead(200, {
+          'Content-Type':'application/json',
+          'Access-Control-Allow-Origin':'*'
       });
+      response.write(JSON.stringify(result));
+      response.end();
+      client.close();
     });
-    /*var reqURI = url.parse("http://" + request.headers.host + request.url);
-    var params = reqURI.query;
-    var terms = params.substr(params.indexOf('=') + 1);
-    mdb.open(function (err, db) {
-        db.command(
-            {
-                'text'   : 'storeinfo',    // Command to execute :  on collection
-                'search' : terms,       // Search term to query with
-                'limit'  : 5000         // Max number of results to return
-            },
-            function (err, obj) {
-                var res = {
-                    'data'   : (err ? null : (obj.results ? obj.results : null)),
-                    'error'  : (err ? err : null),
-                    'message': (err ? err.message : 'JSON response for a tweets query'),
-                    'stats'  : obj.stats,
-                    'terms'  : terms
-                };
-                response.writeHead(200, {
-                    'Content-Type':'application/json',
-                    'Access-Control-Allow-Origin':'*'
-                });
-                response.write(JSON.stringify(res));
-                response.end();
-                db.close();
-            });
-    });*/
 };
 
-/*var mdbServer = new mongo.Server('localhost', 27017, {'auto_reconnect' : true});
-var mdb = mongo.Db('CS432FP', mdbServer);*/
-
 http.createServer(router).listen(8080);
-
-/*http.createServer(router) {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Sneed!');
-}).listen(8081);
-*/
